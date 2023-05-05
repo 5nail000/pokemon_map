3af22f65153a0d05ed010216d4fc84502a4943a4
5nail000 <105508594+5nail000@users.noreply.github.com>
2023-05-05 15:59:42 +0300

    add specs to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,4 +1,5 @@
 from django.db import models  # noqa F401
+import django.utils.timezone
 
 
 class Pokemon(models.Model):
@@ -16,10 +17,14 @@ class Pokemon(models.Model):
 
 class Entities(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    level = models.IntegerField()
     lat = models.DecimalField(max_digits=8, decimal_places=6)
     lon = models.DecimalField(max_digits=8, decimal_places=6)
-    appearted_date = models.DateField(auto_now_add=True)
-    appearted_time = models.TimeField(auto_now_add=True)
-    disappearted_date = models.DateField(auto_now_add=True)
-    disappearted_time = models.TimeField(auto_now_add=True)
+    appearted_date = models.DateField(default=django.utils.timezone.now)
+    appearted_time = models.TimeField(default=django.utils.timezone.now)
+    disappearted_date = models.DateField(default=django.utils.timezone.now)
+    disappearted_time = models.TimeField(default=django.utils.timezone.now)
+    level = models.IntegerField(default=1)
+    health = models.IntegerField(default=100)
+    strength = models.IntegerField(default=100)
+    defence = models.IntegerField(default=100)
+    stamina = models.IntegerField(default=100)
