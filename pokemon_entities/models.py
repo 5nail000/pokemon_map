b6b457c74e09ef47c9bf29c9611407455f73234e
5nail000 <105508594+5nail000@users.noreply.github.com>
2023-05-06 02:51:49 +0300

    adds blanks & nulls

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,10 +3,10 @@ import django.utils.timezone
 
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=200, verbose_name="Имя")
-    title_en = models.CharField(max_length=200, verbose_name="Имя(на английском)")
-    title_jp = models.CharField(max_length=200, verbose_name="Имя(на японском)")
-    description = models.TextField(verbose_name="Описание")
+    title_ru = models.CharField(max_length=200, verbose_name="Имя", blank=False)
+    title_en = models.CharField(max_length=200, verbose_name="Имя(на английском)", blank=True)
+    title_jp = models.CharField(max_length=200, verbose_name="Имя(на японском)", blank=True)
+    description = models.TextField(verbose_name="Описание", blank=True)
     image = models.ImageField(default=None, upload_to='pokemons', blank=True, verbose_name="Изображение")
     next_evolution = models.ForeignKey('self', on_delete=models.SET_NULL,
                                        null=True,
@@ -24,15 +24,15 @@ class Pokemon(models.Model):
 
 
 class Entities(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name="Имя Покемона")
-    lat = models.DecimalField(max_digits=8, decimal_places=6, verbose_name="Координата-широта")
-    lon = models.DecimalField(max_digits=8, decimal_places=6, verbose_name="Координата-долгота")
-    appearted_date = models.DateField(default=django.utils.timezone.now, verbose_name="Дата явления")
-    appearted_time = models.TimeField(default=django.utils.timezone.now, verbose_name="Время явления")
-    disappearted_date = models.DateField(default=django.utils.timezone.now, verbose_name="Дата исчезновения")
-    disappearted_time = models.TimeField(default=django.utils.timezone.now, verbose_name="Время исчезновения")
-    level = models.IntegerField(default=1, verbose_name="Уровень")
-    health = models.IntegerField(default=100, verbose_name="Здоровье")
-    strength = models.IntegerField(default=100, verbose_name="Сила")
-    defence = models.IntegerField(default=100, verbose_name="Защита")
-    stamina = models.IntegerField(default=100, verbose_name="Выносливость")
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name="Имя Покемона", blank=False)
+    lat = models.DecimalField(max_digits=8, decimal_places=6, verbose_name="Координата-широта", blank=False)
+    lon = models.DecimalField(max_digits=8, decimal_places=6, verbose_name="Координата-долгота", blank=False)
+    appearted_date = models.DateField(default=django.utils.timezone.now, verbose_name="Дата явления", blank=True)
+    appearted_time = models.TimeField(default=django.utils.timezone.now, verbose_name="Время явления", blank=True)
+    disappearted_date = models.DateField(default=django.utils.timezone.now, verbose_name="Дата исчезновения", blank=True)
+    disappearted_time = models.TimeField(default=django.utils.timezone.now, verbose_name="Время исчезновения", blank=True)
+    level = models.IntegerField(default=1, verbose_name="Уровень", blank=False)
+    health = models.IntegerField(default=100, verbose_name="Здоровье", blank=False)
+    strength = models.IntegerField(default=100, verbose_name="Сила", blank=False)
+    defence = models.IntegerField(default=100, verbose_name="Защита", blank=False)
+    stamina = models.IntegerField(default=100, verbose_name="Выносливость", blank=False)
