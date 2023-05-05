1d4ccad464f4bc091923f58477fb0630349501a1
5nail000 <105508594+5nail000@users.noreply.github.com>
2023-05-05 13:28:07 +0300

    create Pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,19 @@
 from django.db import models  # noqa F401
 
-# your models here
+
+class Pokemon(models.Model):
+    pokemon_id = models.IntegerField()
+    title_ru = models.CharField(max_length=200)
+    title_en = models.CharField(max_length=200)
+    title_jp = models.CharField(max_length=200)
+    description = models.TextField()
+    img_url = models.CharField(max_length=2048)
+    next_evolution = models.IntegerField(default=None)
+    previous_evolution = models.IntegerField(default=None)
+
+
+class Entities(models.Model):
+    pokemon_id = models.IntegerField()
+    level = models.IntegerField()
+    lat = models.DecimalField(max_digits=8, decimal_places=6)
+    lon = models.DecimalField(max_digits=8, decimal_places=6)
